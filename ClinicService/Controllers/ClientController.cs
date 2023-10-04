2ed4fc5b8d8f367e0027af68466c0d6d62390868
Kate <you-like-me@yandex.ru>
2023-10-04 12:14:00 +0300

    Добавьте файлы проекта.

---
+++ ClinicService/Controllers/ClientController.cs
@@ -0,0 +1,70 @@
+﻿using ClinicService.Models.Requests;
+using ClinicService.Models;
+using ClinicService.Services;
+using ClinicService.Services.Impl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+
+        private readonly IClientRepository _clientRepository;
+
+        public ClientController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] CreateClientRequest createRequest)
+        {
+            int res = _clientRepository.Create(new Client
+            {
+                Document = createRequest.Document,
+                SurName = createRequest.SurName,
+                FirstName = createRequest.FirstName,
+                Patronymic = createRequest.Patronymic,
+                Birthday = createRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] UpdateClientRequest updateRequest)
+        {
+            int res = _clientRepository.Update(new Client
+            {
+                ClientId = updateRequest.ClientId,
+                SurName = updateRequest.SurName,
+                FirstName = updateRequest.FirstName,
+                Patronymic = updateRequest.Patronymic,
+                Birthday = updateRequest.Birthday
+            });
+            return Ok(res);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] int clientId)
+        {
+            int res = _clientRepository.Delete(clientId);
+            return Ok(res);
+        }
+
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            return Ok(_clientRepository.GetAll());
+        }
+
+
+        [HttpGet("get/{clientId}")]
+        public IActionResult GetById([FromRoute] int clientId)
+        {
+            return Ok(_clientRepository.GetById(clientId));
+        }
+    }
+}
